smemsh/taskwtools

commit 2c56ea91ad489f774d60de61ae877327429da621

+ tasklabels: invocations like taskfql/taskfqls but only labels
this and the previous few commits have re-used the taskfql machinery but
ferried through a flag to return and print the last fql pathcomponent
only, ie the label.  all we need to implement this are symlinks, the
call name indirection, and the [new] flag labelonly=True

task.py
@@ -165,12 +165,15 @@ def _taskone(*args, **kwargs):
 
 #
 
-def __taskfql(task):
+def __taskfql(task, labelonly=False):
     prj = task.get('project')
-    try:
-        if prj: return f"{prj.replace('.', '/')}/{task['label']}"
-    except KeyError:
-        bomb("at least one task does not have a label")
+    label = task.get('label')
+    if not prj or not label:
+        bomb("at least one task does not have a project or label")
+    if labelonly:
+        return label
+    else:
+        return f"{prj.replace('.', '/')}/{label}"
 
 def _taskfql(*args, **kwargs):
     task = _taskone(*args, **kwargs)
@@ -187,6 +190,12 @@ def taskfql(*args, **kwargs):
 def taskfqls(*args, **kwargs):
     for t in _taskfqls(*args, **kwargs):
         print(t or '')
+
+def tasklabels(*args):
+    taskfqls(*args, labelonly=True)
+
+def tasklabel(*args):
+    taskfql(*args, labelonly=True)
 
 #
 

tasklabel
@@ -1,0 +1,1 @@
+task.py

tasklabels
@@ -1,0 +1,1 @@
+./task.py

commit 4730a1b05a7363d67db43ff0dd2a571051722d49

/ taskfql: fix onmodify hook by returning None if fql malformed
during taskadd, the old task will be one without a project or label, and
that has to work, so fql has to return nothing and we'll bypass further
hook processing

this broke in 2c56ea9 when we stopped gating checks on a label with
existence of a project.  but really the hook should just not run if both
are present, rather than thinking this is an error.  all control for new
tasks using task.py scripts should be happening through taskadd, so
there should never be a malformed one created unless it was
intentionally bypassing the task.py scripts anyways

task.py
@@ -169,12 +169,9 @@ def _taskone(*args, **kwargs):
 def __taskfql(task, labelonly=False):
     prj = task.get('project')
     label = task.get('label')
-    if not prj or not label:
-        bomb("at least one task does not have a project or label")
-    if labelonly:
-        return label
-    else:
-        return f"{prj.replace('.', '/')}/{label}"
+    if not prj or not label: return # taskadd or nonconforming
+    if labelonly: return label
+    return f"{prj.replace('.', '/')}/{label}"
 
 def _taskfql(*args, **kwargs):
     task = _taskone(*args, **kwargs)